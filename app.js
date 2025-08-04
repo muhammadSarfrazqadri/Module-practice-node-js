7851f2798d151416285c7307f5eb6c3292087cf8
Muhammad Sarfraz <msowner.best@gmail.com>
2025-08-04 10:42:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const add = require('./add');
 const subtract = require('./subtract');
 const multiply = require('./multiply');
 const divide = require('./divide');
+const result = document.getElementById("result")
+const num1 = document.getElementById("num1")
+const num2 = document.getElementById("num2")
 
+module.export = {num1 , num2, result}
 add(10, 5);
 subtract(10, 5);
 multiply(10, 5);
