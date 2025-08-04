24e0944f6352e34db99d8c62c8d288d1f7055f6d
Muhammad Sarfraz <msowner.best@gmail.com>
2025-08-04 10:26:42 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+add(10, 5);
+subtract(10, 5);
+multiply(10, 5);
+divide(10, 5);
+divide(10, 0);
