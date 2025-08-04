a02c68563ccccbc4e7e651aae128cbae786fc587
Muhammad Sarfraz <msowner.best@gmail.com>
2025-08-04 11:28:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,9 @@ const add = require('./add');
 const subtract = require('./subtract');
 const multiply = require('./multiply');
 const divide = require('./divide');
-const result = document.getElementById("result")
-const num1 = document.getElementById("num1")
-const num2 = document.getElementById("num2")
-const addBtn = document.getElementById('addBtn')
-const divBtn = document.getElementById('addBtn')
-const subBtn = document.getElementById('addBtn')
-const mulBtn = document.getElementById('addBtn')
 
-module.export = {num1 , num2, result}
-
-addBtn.addEventListener(click, add(num1,num2));
-divBtn.addEventListener(click, divide(num1,num2));
-subBtn.addEventListener(click, subtract(num1,num2));
-mulBtn.addEventListener(click, multiply(num1,num2));
+add(10, 5);
+subtract(10, 5);
+multiply(10, 5);
+divide(10, 5);
+divide(10, 0); // Division by zero test
