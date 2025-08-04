a7c40eec5d01a6a984abb2ef8fa82936fe6930f0
Muhammad Sarfraz <msowner.best@gmail.com>
2025-08-04 11:14:51 +0500

    Update add.js

--- add.js
+++ add.js
@@ -2,6 +2,9 @@ const {num1 , num2 ,result} = require(./app.js)
 
 function add(a, b) {
   result.innerText = `Addition of ${a} and ${b} is ${a + b}`;
+  num1.style.display = 'none'
+  num2.style.display = 'none'
+  result.style.display = 'block'
 }
 
 module.exports = add;
